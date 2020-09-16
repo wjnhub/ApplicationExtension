2d07bff4692cebc1e2fdfb153be6a0a65b7ec605
wjn <wjnhub@163.com>
2020-09-16 10:54:12 +0800

    init

---
+++ ApplicationExtension/ApplicationExtension/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplicationExtension
+//
+//  Created by wjn on 2020/9/16.
+//  Copyright © 2020 wjn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
